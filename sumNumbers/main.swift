0fdf960ac9be0cbc246c18db26c58cc2b2a03028
Markus Lindemann <lindemannm@hotmail.com>
2016-06-20 20:50:23 +0100

    Initial Commit

---
+++ sumNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sumNumbers
+//
+//  Created by Markus Lindemann on 20/06/2016.
+//  Copyright © 2016 Markus Lindemann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
